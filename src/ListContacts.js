e6ea912237b7014386135fce65c207b163e5bb20
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-12-23 01:31:49 +0530

    Add input for updating list

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -8,9 +8,29 @@ class ListContacts extends Component {
         onDeleteContact: PropTypes.func.isRequired
     }
 
+    state = {
+        query : ''
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
     render() {
         return (
-            <ol className="contact-list" >
+            <div className="list-contacts" >
+                <div className="list-contacts-top" >
+                    <input 
+                        className="search-contacts"
+                        type="text"
+                        placeholder="Search Contacts"
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+                <ol className="contact-list" >
                 {
                     this.props.contacts.map((contact) => (
                         <li key={contact.id} className="contact-list-item" >
@@ -32,7 +52,8 @@ class ListContacts extends Component {
                         </li>
                     ))
                 }
-            </ol>
+                </ol>
+            </div>
         )
     }
 }
